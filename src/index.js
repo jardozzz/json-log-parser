f3dd1f7df7fbf49bf3d474113b2e67c3f7d8931e
jardozzz <79194712+jardozzz@users.noreply.github.com>
2022-07-28 15:46:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ const reg = /"Date":.+?"BooksInfos":\[.+?}}}]}]/g;
 const regBook = /"BooksInfos":\[.+?}}}]}]/g;
 const regDate = /[0-9].+?[0-9]"/;
 //const regdate = /"Date":.+?[0-9]"/;
-const arrHolder = [];
+
 let bookiesID = {
   1: 'pinnacle',
   6: 'fonbet',
   110: 'ggbet',
   17: 'parimatch',
+  27: 'Olimp',
+  79: 'Sportsbook',
+  19: 'Unibet',
 };
 function parse(q) {
+  const arrHolder = [];
   let j = q.match(reg).map((e) => {
     return [
       e.match(regDate)[0].slice(0, -1),
@@ -24,13 +28,13 @@ function parse(q) {
       for (let i in e.odds) {
         if (
           arrHolder.find(
-            (h) => h.market === e.market && h.period === e.period && h.variant===e.variant
+            (h) => h.market === e.market && h.period === e.period
           ) == undefined
         ) {
-          arrHolder.push({ market: e.market, period: e.period,variant:e.variant, arr: [] });
+          arrHolder.push({ market: e.market, period: e.period, arr: [] });
         }
         arr = arrHolder.find(
-          (h) => h.market === e.market && h.period === e.period && h.variant===e.variant
+          (h) => h.market === e.market && h.period === e.period
         ).arr;
 
         if (!arr[index]) {
@@ -51,18 +55,58 @@ function parse(q) {
           elem.win2 = e.odds[i].odd;
         }
         if (e.selectionId === 3) elem.draw = e.odds[i].odd;
-        if (e.selectionId === 34) {
-          elem.variant=e.variant
-          elem.win1=e.odds[i].odd
-          const backhb=element[1].BooksInfos[0].attached.find((q)=>q.selectionId===35 && q.variant===`${-e.variant}`)
-          elem.win2= backhb?backhb.odds[i].odd:""
+        if (e.selectionId === 34 || e.selectionId === 7) {
+          if (!elem.variants) elem.variants = [];
+          console.log(elem.variants);
+          if (
+            !elem.variants.some(
+              (h) => h.variant === `${-Number(e.variant)}` && h.hb === 'HB2'
+            )
+          ) {
+            elem.variants.push({ variant: `${Number(e.variant)}`, hb: 'HB1' });
+          }
+
+          if (
+            elem.variants.find(
+              (h) => h.variant === `${Number(e.variant)}` && h.hb === 'HB1'
+            ) === undefined
+          ) {
+            elem.variants.find(
+              (h) => h.variant === `${-Number(e.variant)}` && h.hb === 'HB2'
+            ).win1 = e.odds[i].odd;
+          } else
+            elem.variants.find(
+              (h) => h.variant === `${Number(e.variant)}` && h.hb === 'HB1'
+            ).win1 = e.odds[i].odd;
         }
-               if (e.odds[i].operation === 'SUSPENDED') elem.status = 'SUSPENDED';
+        if (e.selectionId === 35 || e.selectionId === 8) {
+          if (!elem.variants) elem.variants = [];
+          if (
+            !elem.variants.some(
+              (h) => h.variant === `${-Number(e.variant)}` && h.hb === 'HB1'
+            )
+          ) {
+            elem.variants.push({ variant: `${Number(e.variant)}`, hb: 'HB2' });
+          }
+          if (
+            elem.variants.find(
+              (h) => h.variant === `${Number(e.variant)}` && h.hb === 'HB2'
+            ) === undefined
+          ) {
+            elem.variants.find(
+              (h) => h.variant === `${-Number(e.variant)}` && h.hb === 'HB1'
+            ).win2 = e.odds[i].odd;
+          } else
+            elem.variants.find(
+              (h) => h.variant === `${Number(e.variant)}` && h.hb === 'HB2'
+            ).win2 = e.odds[i].odd;
+        }
+        if (e.odds[i].operation === 'SUSPENDED') elem.status = 'SUSPENDED';
         arr[index].date = element[0];
       }
     });
   });
-
+  console.log(arrHolder);
   return arrHolder;
 }
 const goButton = document.querySelector('.parser');
@@ -75,7 +119,7 @@ goButton.addEventListener('click', () => {
   parse(str.value).forEach((e) => {
     let but = document.createElement('button');
 
-    but.innerHTML = e.period + e.market+(e.variant ||"");
+    but.innerHTML = e.period + e.market;
     but.addEventListener('click', () => drawOdds(e));
     container.appendChild(but);
   });
@@ -88,11 +132,17 @@ function drawOdds(array) {
     const temp = document.querySelector('#container').content.cloneNode(true);
     temp.querySelector('.date').innerHTML = e.date;
     e.bookies.forEach((e) => {
+      let innertexT = `${e.book}\n`;
+      if (e.variants) {
+        e.variants.forEach((hb) => {
+          innertexT += `${hb.variant} ${hb.win1} ${hb.win2}\n`;
+        });
+      } else {
+        innertexT += `${e.win1} ${e.draw || ''}  ${e.win2}\n `;
+      }
+
       const bookmaker = document.createElement('span');
-      bookmaker.innerText = `${e.book}\n
-  ${e.win1.variant ? e.win1.variant + e.win1.line : e.win1}  ${
-        e.draw || ''
-      }    ${e.win2.variant ? e.win2.variant + e.win2.line : e.win2} \n`;
+      bookmaker.innerText = innertexT;
       if (e.status === 'SUSPENDED') {
         bookmaker.classList.add('redborder');
       }
